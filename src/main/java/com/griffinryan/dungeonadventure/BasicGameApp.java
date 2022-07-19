d98aed99872762091550ce50e3b2a8675d4ca8b5
griffinryan <glryan@uw.edu>
2022-07-19 15:44:16 -0700

    basic game engine now established with the FXGL library. Handles player input and renders at 60 fps

--- src/main/java/com/griffinryan/dungeonadventure/BasicGameApp.java
+++ src/main/java/com/griffinryan/dungeonadventure/BasicGameApp.java
@@ -14,16 +14,61 @@ import java.util.Map;
 
 public class BasicGameApp extends GameApplication {
 
+	private Entity player;
+
 	public static void main(String[] args){
 		launch(args);
 	}
 
 	@Override
 	protected void initSettings(GameSettings settings){
-
 		settings.setWidth(600);
 		settings.setHeight(600);
 		settings.setTitle("Dungeon Adventure");
 		settings.setVersion("0.1");
+	}
+
+	@Override
+	protected void initGame(){
+		player = FXGL.entityBuilder().at(300,300)
+				.view(new Rectangle(25,25,Color.DODGERBLUE))
+				.buildAndAttach();
+	}
+
+	@Override
+	protected void initInput(){
+
+		FXGL.onKey(KeyCode.D, () -> {
+			player.translateX(5); // Move right.
+			FXGL.inc("pixelsMoved", +5);
+		});
+
+		FXGL.onKey(KeyCode.A, () -> {
+			player.translateX(-5); // Move left.
+		});
+
+		FXGL.onKey(KeyCode.W, () -> {
+			player.translateY(-5); // Move up.
+		});
+
+		FXGL.onKey(KeyCode.S, () -> {
+			player.translateY(5); // Move down.
+		});
+	}
+
+	@Override
+	protected void initGameVars(Map<String, Object> vars) {
+		vars.put("pixelsMoved", 0);
+	}
+
+	@Override
+	protected void initUI(){
+		Text textPixels = new Text();
+		textPixels.setTranslateX(50); // x = 50
+		textPixels.setTranslateY(100); // y = 100
+
+		textPixels.textProperty().bind(FXGL.getWorldProperties().intProperty("pixelsMoved").asString());
+
+		FXGL.getGameScene().addUINode(textPixels); // add to the scene graph
 	}
 }
