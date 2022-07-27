205e984b073df215ca2aa89a74b04b4693c09df7
griffinryan <glryan@uw.edu>
2022-07-27 13:48:39 -0700

    implementing BackgroundComponent

--- src/main/java/com/griffinryan/dungeonadventure/engine/EntityType.java
+++ src/main/java/com/griffinryan/dungeonadventure/engine/EntityType.java
@@ -1,13 +1,17 @@
 package com.griffinryan.dungeonadventure.engine;
 
 /**
- * 
+ *
  * @author Griffin Ryan (glryan@uw.edu)
- * @author Yudong Lin (ydlin@uw.edu) 
+ * @author Yudong Lin (ydlin@uw.edu)
  * @author Elijah Amian (elijah25@uw.edu)
  */
 public enum EntityType {
 	ENEMY, PLAYER, POTION,
-	// PARTICLE_LAYER, GRID
-	LEVEL, BACKGROUND
+
+	LEVEL, BACKGROUND,
+
+	DOOR, ROOM, DUNGEON
+
+	// PARTICLE, GRID, MAZE
 }
---
+++ src/main/java/com/griffinryan/dungeonadventure/engine/component/DoorComponent.java
@@ -0,0 +1,10 @@
+package com.griffinryan.dungeonadventure.engine.component;
+
+/**
+ *
+ * @author Griffin Ryan (glryan@uw.edu)
+ * @author Yudong Lin (ydlin@uw.edu)
+ * @author Elijah Amian (elijah25@uw.edu)
+ */
+public class DoorComponent {
+}
